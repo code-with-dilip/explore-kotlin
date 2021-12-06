dcba67646b18ebc603db5ff36c28c2290387230e
Dilip Sundarraj <you@example.com>
2021-12-05 19:28:04 -0600

    added the code lamdas;

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/collections/ExploreCollections.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/collections/ExploreCollections.kt
@@ -1,6 +1,6 @@
 package com.kotlinplayground.collections
 
-
+import com.kotlinplayground.dataset.courseList
 
 
 fun main() {
@@ -13,10 +13,10 @@ fun main() {
     println("namesMutableList : $namesMutableList")
 
 
-    val nameAgeMap = hashMapOf("dilip" to  33 ,"scooby"  to  4)
+    val nameAgeMap = hashMapOf("dilip" to 33, "scooby" to 4)
     println("nameAgeMap : $nameAgeMap")
 
-    val nameAgeMutableMap = mutableMapOf("dilip" to  33 ,"scooby"  to  4)
+    val nameAgeMutableMap = mutableMapOf("dilip" to 33, "scooby" to 4)
     nameAgeMutableMap.put("abc", 100)
     println("nameAgeMutableMap : $nameAgeMutableMap")
 
@@ -25,8 +25,34 @@ fun main() {
 
     val mutableSet = mutableSetOf("adam", "ben", "chloe")
     mutableSet.add("adam1")
-    println("mutableSet :  $mutableSet" )
+    println("mutableSet :  $mutableSet")
+
+    val add = { x: Int -> x + x }
+
+    fun add(x:Int) = x+x
+
+    listOf(1, 2, 3)
+        .forEach {
+            val result = add(it)
+            println(result)
+        }
+
+    listOf(1, 2, 3)
+        .forEach {
+            val result = add(it)
+            println(result)
+        }
 
 
+    val courses = courseList()
+    courses
+        .forEach { course ->
+         //   println("course : $course")
+        }
+
+    courses
+        .forEach {
+        //    println("it : $it")
+        }
 
 }
